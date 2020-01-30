3a2f5e5a923bd1e566435921b587d4b0df174a74
CHRISTOPHER HERNKE <CHRHERNK@uat.edu>
2020-01-30 13:59:37 -0700

    Added the Linear Code

--- HK WARGAMES/Source.cpp
+++ HK WARGAMES/Source.cpp
@@ -18,14 +18,15 @@ int main()
 	int AI_Bianary_searchGridLowNumber = 1;
 	int AI_Bianary_searchGridHighNumber = 64;
 
-
-
-	
 	int Human_searchGridLowNumber = 1;
 	int Human_searchGridHighNumber = 64;
 	// keep the target location the same for all parties
 
+	int AI_Linear_searchGridLowNumber = 1;
+	int AI_Linear_searchGridHighNumber = 64;
 
+	int AI_Random_searchGridLowNumber = 1;
+	int AI_Random_searchGridHighNumber = 64;
 
 	
 	int actualTargetlocation = rand() % ((AI_Bianary_searchGridHighNumber - AI_Bianary_searchGridLowNumber) / 2 + AI_Bianary_searchGridLowNumber);
@@ -34,16 +35,15 @@ int main()
 	int AI_Bianary_Number_Of_Tries = 0;
 	int AI_Bianary_Current_Location_Prediction = 0;
 
-
-
-	
 	int Human_Number_Of_Tries = 0;
 	int Human_Current_Location_Prediction = 0;
 
+	int AI_Linear_Number_Of_Tries = 0;
+	int AI_Linear_Current_Location_Prediction = 0;
 
-
-
-
+	int AI_Random_Number_Of_Tries = 0;
+	int AI_Random_Current_Location_Prediction = 0;
+	//vars that track both the tries, and current guesses
 	
 	cout << "\tWelcome to Search and Rescue\n\n";
 	//your title for the simulation here
@@ -93,6 +93,8 @@ int main()
 		}
 	} while (Human_Current_Location_Prediction != actualTargetlocation);
 	//**********************END OF Human Human tries to find target**********************************************
+
+	
 	cout << " The human has finished! Now launching the Binary AI \n\n";
 
 
@@ -109,7 +111,7 @@ int main()
 
 		AI_Bianary_Current_Location_Prediction = ((AI_Bianary_searchGridHighNumber - AI_Bianary_searchGridLowNumber) / 2 + AI_Bianary_searchGridLowNumber);
 
-		++AI_Bianary_Number_Of_Tries;
+		++ AI_Bianary_Number_Of_Tries;
 
 		//adds a try to the counter
 		if (AI_Bianary_Current_Location_Prediction > actualTargetlocation)
@@ -139,7 +141,46 @@ int main()
 	} while (AI_Bianary_Current_Location_Prediction != actualTargetlocation);
 	//**********************Bianary AI search algorythum tries to find target**********************************************
 
+	cout << "Launch the Linear AI Search";
+	system("Pause");
 
+	
+	//*********************************linear AI search algorythum tries to find target**************************************************
+	do
+	{
+		//this is where the AI makes a target Guess
+
+		AI_Linear_Current_Location_Prediction++;
+		//algo to make the predictions
+		cout << "\n\n The Linear AI guesses the target is at location : " << AI_Linear_Current_Location_Prediction;
+
+		cout << "\n\nThe real target is at : " << actualTargetlocation;
+
+
+		++AI_Linear_Number_Of_Tries;
+
+		//adds a try to the counter
+		if (AI_Linear_Current_Location_Prediction > actualTargetlocation)
+		{
+
+
+			cout << "The AI Prediction is too high!\n\n";
+		}
+
+		else if (AI_Linear_Current_Location_Prediction < actualTargetlocation)
+		{
+
+			cout << "The Prediction was Too Low!!!\n\n";
+		}
+
+		else
+		{
+
+			cout << "\n Thats it!! Linear AI got The package back in " << AI_Linear_Number_Of_Tries << " Guesses!!\n";
+
+		}
+	} while (AI_Linear_Current_Location_Prediction != actualTargetlocation);
+	//**********************linear AI search algorythum found target**********************************************
 
 
 	return 0;
